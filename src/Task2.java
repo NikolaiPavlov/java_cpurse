36d3076986677a6159644951fea6908d3207ea5e
NikolaiPavlov <luker1998@yandex.ru>
2023-05-04 19:02:04 +0300

    add task2

---
+++ src/Task2.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+public class Task2 {
+    public static void main(String[] args) {
+        /*
+        Дан массив двоичных чисел, например [1,1,0,1,1,1],
+        вывести максимальное количество подряд идущих 1.
+         */
+        int[] arr = new int[]{1, 1, 0, 1, 1, 1};
+        int val = 1;
+        int maxCount = 0;
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == val){
+                count++;
+            } else {
+                if (count > maxCount){
+                    maxCount = count;
+                }
+                count = 0;
+            }
+        }
+        if (count > maxCount){
+            maxCount = count;
+        }
+        System.out.println(maxCount);
+    }
+}
