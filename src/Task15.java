76e57bdce71f9cc73c24cc1137fc5529b0236cfb
NikolaiPavlov <luker1998@yandex.ru>
2023-05-08 17:51:31 +0300

    add task15

---
+++ src/Task15.java
@@ -0,0 +1,75 @@
+/*
+1. Дана json строка [{ "фамилия":"Иванов","оценка":"5","предмет":"Математика"},{"фамилия":"Петрова","оценка":"4","предмет":"Информатика"},{"фамилия":"Краснов","оценка":"5","предмет":"Физика"}]
+Задача написать метод(ы), который распарсит строку и выдаст ответ вида:
+Студент Иванов получил 5 по предмету Математика.
+Студент Петрова получил 4 по предмету Информатика.
+Студент Краснов получил 5 по предмету Физика.
+
+Используйте StringBuilder для подготовки ответа
+
+Исходная json строка это просто String !!! Для работы используйте методы String, такие как replace, split, substring и т.д. по необходимости
+
+Создать метод, который запишет результат работы в файл. Обработайте исключения и запишите ошибки в лог файл.
+ */
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+public class Task15 {
+    public static void main(String[] args) throws IOException {
+        saveToFile(parse(), "test.txt");
+    }
+
+    static String parse() {
+        String res = "";
+        Map<String, String> map = new HashMap<>();
+
+        String s = "{{\"фамилия\":\"Иванов\",\"оценка\":\"5\",\"предмет\":\"Математика\"}," +
+                "{\"фамилия\":\"Петрова\",\"оценка\":\"4\",\"предмет\":\"Информатика\"}," +
+                "{\"фамилия\":\"Краснов\",\"оценка\":\"5\",\"предмет\":\"Физика\"}}";
+        String[] arr = s.replace("{", "").
+                replace("\"", "").
+                split("},");
+
+        for (String data : arr) {
+            String[] keys = data.split(",");
+            for (String keyValues : keys) {
+                String[] keyValue = keyValues.replace("}", "").split(":");
+                String key = keyValue[0];
+                String value = keyValue[1];
+                map.put(key, value);
+            }
+            res += "Студент " + map.get("фамилия") + " получил " +
+                    map.get("оценка") + " по предмету " + map.get("предмет") + ".\n";
+        }
+        return res;
+    }
+
+    static void saveToFile(String s, String filePath) throws IOException {
+        Logger logger = Logger.getAnonymousLogger();
+        SimpleFormatter formatter = new SimpleFormatter();
+        FileHandler fileHandler = null;
+        try {
+            fileHandler = new FileHandler("log.txt");
+            fileHandler.setFormatter(formatter);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        logger.addHandler(fileHandler);
+
+        StringBuilder stringBuilder = new StringBuilder(s);
+        try (FileWriter fileWriter = new FileWriter(filePath, false)) {
+            fileWriter.write(stringBuilder.toString());
+        } catch (Exception e){
+            logger.log(Level.WARNING, e.getMessage());
+            fileHandler.setFormatter(formatter);
+        }
+        fileHandler.close();
+    }
+}
