69fa5053f706c22653cfb541912fff2240136bc8
NikolaiPavlov <luker1998@yandex.ru>
2023-05-09 17:14:45 +0300

    solve problem

--- src/homework_2/Task16.java
+++ src/homework_2/Task16.java
@@ -2,60 +2,27 @@ package homework_2;/*
 2. *Получить исходную json строку из файла, используя FileReader или Scanner
  */
 
-import java.io.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Task16 {
-    public static void main(String[] args) throws Exception {
-        String templateQuery = "Студент ";
-        String content = getString("test.txt");
-        System.out.println(content);
-        content = content.substring(1, content.length() - 1);
-        content = content.replaceAll("[\"\\s{]", "");
-        content = content.replaceAll("},", "!");
-        content = content.replaceAll("}", "");
-        String[] lines = content.split("!");
-        for (String line : lines) {
-            String query = new StringBuilder(templateQuery).append(parseLines(line)).toString();
-            System.out.print(query);
-        }
+    public static void main(String[] args) {
+        System.out.println(readFile("test.txt"));
     }
 
-    public static String getString(String fileName) {
-        String content = null;
+    public static String readFile(String fileName) {
+        File file = new File(fileName);
+        Scanner scanner = null;
         try {
-            File file = new File(fileName);
-            FileReader fr = new FileReader(file);
-            BufferedReader reader = new BufferedReader(fr);
-            content = reader.readLine();
-
-            reader.close();
+            scanner = new Scanner(file);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
         }
-
-        return content;
-    }
-    public static String parseLines(String myStr) {
-        String[] myStrArr = myStr.split(",");
-        StringBuilder sb = new StringBuilder();
-        for (String criterion : myStrArr) {
-            String[] keyValue = criterion.split(":");
-            if (keyValue[0].equals("фамилия")) {
-                sb.append(keyValue[1]);
-                sb.append(" получил ");
-            }
-            if (keyValue[0].equals("оценка")) {
-                sb.append("оценку ");
-                sb.append(keyValue[1]);
-            }
-            if (keyValue[0].equals("предмет")) {
-                sb.append(" по предмету ");
-                sb.append(keyValue[1]);
-                sb.append(". ");
-            }
+        String res = "";
+        while (scanner.hasNext()) {
+            res += scanner.nextLine() + "\n";
         }
-        return sb.toString();
+        return res;
     }
 }
